4195c6780c03ff51da3f0afb8226e9ffe9c94f5e
anshshr <anshshrivastav24@gmail.com>
2025-04-19 01:26:19 +0530

    added the requirements file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import "package:flutter/material.dart";
 import "package:multilingual_chat_bot/pages/record_audio.dart";
+import "package:multilingual_chat_bot/services/notification_service.dart";
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
